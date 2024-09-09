d9b42a5c4601fbd5387e47e6e9ded09545f3de2e
adam <adam.cottis@outlook.com>
2024-09-09 22:32:18 +0100

    Frame buffer

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const uefi = std.os.uefi;
+const cpu = @import("cpu.zig");
 
 const Writer = struct {
     inner: *uefi.protocol.SimpleTextOutput,
@@ -27,32 +28,51 @@ const Writer = struct {
     }
 };
 
-pub fn main() uefi.Status {
-    const writer = Writer.init(uefi.system_table.con_out.?);
-
-    writer.clear();
-    writer.print_comptime("hi\r\n");
-
-    const mmap_desc_entries = 1000;
-    var mmap_size: usize = @sizeOf(uefi.tables.MemoryDescriptor) * mmap_desc_entries;
-    const mmap_descs: ?[*]uefi.tables.MemoryDescriptor = &[_]uefi.tables.MemoryDescriptor{std.mem.zeroInit(uefi.tables.MemoryDescriptor, .{})} ** mmap_desc_entries;
-    var map_key: usize = 0;
+fn get_memory_map(mem_map_key: *usize) uefi.Status {
+    const mem_map_desc_entries = 1000;
+    var mem_map_size: usize = @sizeOf(uefi.tables.MemoryDescriptor) * mem_map_desc_entries;
+    const mem_map_descs: ?[*]uefi.tables.MemoryDescriptor = &[_]uefi.tables.MemoryDescriptor{std.mem.zeroInit(uefi.tables.MemoryDescriptor, .{})} ** mem_map_desc_entries;
     var desc_size: usize = 0;
     var desc_version: u32 = 0;
-    const res = uefi.system_table.boot_services.?.getMemoryMap(
-        &mmap_size,
-        mmap_descs,
-        &map_key,
+
+    return uefi.system_table.boot_services.?.getMemoryMap(
+        &mem_map_size,
+        mem_map_descs,
+        mem_map_key,
         &desc_size,
         &desc_version,
     );
+}
+
+pub fn main() uefi.Status {
+    var res: uefi.Status = undefined;
+    const writer = Writer.init(uefi.system_table.con_out.?);
+
+    writer.clear();
+    writer.print_comptime("Welcome to zos!\r\n");
+
+    var graphics_output: *uefi.protocol.GraphicsOutput = undefined;
+    res = uefi.system_table.boot_services.?.locateProtocol(&uefi.protocol.GraphicsOutput.guid, null, @ptrCast(&graphics_output));
+    if (res != uefi.Status.Success) {
+        writer.print("Failed to get graphics output {}\r\n", .{res});
+    }
+    const frame_buffer = graphics_output.mode.frame_buffer_base;
+    writer.print("Frame Buffer Base: 0x{X}\r\n", .{frame_buffer});
+
+    var mem_map_key: usize = 0;
+    res = get_memory_map(&mem_map_key);
     if (res != uefi.Status.Success) {
         writer.print("Failed to read memory map {}\r\n", .{res});
     }
 
-    writer.print("{}", .{desc_version});
+    res = uefi.system_table.boot_services.?.exitBootServices(uefi.handle, mem_map_key);
+    if (res != uefi.Status.Success) {
+        writer.print("Failed to exit boot services {}\r\n", .{res});
+    }
 
-    while (true) {}
+    while (true) {
+        cpu.halt();
+    }
 
     return uefi.Status.Success;
 }
